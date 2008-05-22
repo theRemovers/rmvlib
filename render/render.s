69886cf5509bf7f2c59daacc93bd9f2f2bb263b4
Seb/The Removers <SebRmv@jagware.org>
2008-05-22 14:21:48 +0000

    git-svn-id: svn://obiwan/Jaguar@386 1d14b586-ad98-11dc-b037-dddde97c3f5c

--- render/render.s
+++ render/render.s
@@ -193,6 +193,7 @@ ENABLE_TEXTURE_GOURAUD	equ	1
 	add	r0,r30		; relocate .gouraud_shading_pixel
 	shlq	#1,r29
 	add	r29,r30
+	not	r29
 	jump	(r30)
 	shrq	#1,r29
 .gouraud_phrase_mode\~:	
@@ -224,7 +225,31 @@ ENABLE_TEXTURE_GOURAUD	equ	1
 	store	r26,(r15+((B_IINC-(A1_BASE+12))/4))	; B_IINC
 	.endm
 
-	.macro	set_z
+	.macro	set_i_phrase
+	;; r15 is A1_BASE+32
+	;; r15 equals A1_BASE at the end
+	store	r25,(r15+((B_I3-(A1_BASE+32))/4))
+	sub	r26,r25
+	store	r25,(r15+((B_I2-(A1_BASE+32))/4))
+	sub	r26,r25	
+	store	r25,(r15+((B_I1-(A1_BASE+32))/4))
+	sub	r26,r25
+	shlq	#10,r26
+	store	r25,(r15+((B_I0-(A1_BASE+32))/4))
+	shrq	#8,r26
+	subq	#32,r15
+	store	r26,(r15+((B_IINC-A1_BASE)/4))
+	.endm
+
+	.macro	set_i_pixel
+	;; r15 is A1_BASE
+	shlq	#8,r26
+	shrq	#8,r26
+	store	r25,(r15+((B_I3-A1_BASE)/4)) 	; B_I3
+	store	r26,(r15+((B_IINC-A1_BASE)/4)) 	; B_IINC
+	.endm
+	
+	.macro	set_z_phrase
 	;; r15 is A1_BASE+32
 	store	r25,(r15+((B_Z3-(A1_BASE+32))/4)) 	; B_Z3
 	sub	r26,r25
@@ -235,6 +260,12 @@ ENABLE_TEXTURE_GOURAUD	equ	1
 	shlq	#2,r26					; dz * 4
 	store	r25,(r15+((B_Z0-(A1_BASE+32))/4)) 	; B_Z0
 	store	r26,(r15+((B_ZINC-(A1_BASE+32))/4))	; B_ZINC
+	.endm
+
+	.macro	set_z_pixel
+	;; r15 is A1_BASE+32
+	store	r25,(r15+((B_Z3-(A1_BASE+32))/4))	; B_Z3  
+	store	r26,(r15+((B_ZINC-(A1_BASE+32))/4))	; B_ZINC 
 	.endm
 	
 	.text
@@ -760,23 +791,13 @@ renderer:
 	;; compute i
 	compute_i
 	;;
-	detect_gouraud_mode	.gouraud_shading_pixel
+	detect_gouraud_mode	.gouraud_shading_pixel_mode
 	;; 
 	wait_blitter_gpu	r15,r29
  	or	r21,r28		; 1|w (executed during wait loop)
 	;; set intensities
-	addq	#16,r15
-	store	r25,(r15+((B_I3-(A1_BASE+16))/4))
-	sub	r26,r25
-	store	r25,(r15+((B_I2-(A1_BASE+16))/4))
-	sub	r26,r25	
-	store	r25,(r15+((B_I1-(A1_BASE+16))/4))
-	sub	r26,r25
-	shlq	#10,r26
-	store	r25,(r15+((B_I0-(A1_BASE+16))/4))
-	shrq	#8,r26
-	subq	#16,r15
-	store	r26,(r15+((B_IINC-A1_BASE)/4))
+	addq	#32,r15
+	set_i_phrase
 	;; 
 	movefa	r18,r30					; blitter flags
 	movei	#PATDSEL|GOURD,r29
@@ -789,17 +810,14 @@ renderer:
 	add	r12,r11		; rx += rdx
 	jump	(r18)		; -> .do_scanlines
 	add	r21,r4		; y++
-.gouraud_shading_pixel:
+.gouraud_shading_pixel_mode:
 	;; mandatory!
 	begin_gouraud_pixel
 	;; 
 	wait_blitter_gpu	r15,r29
  	or	r21,r28		; 1|w (executed during wait loop)
 	;; set intensities
-	shlq	#8,r26
-	store	r25,(r15+((B_I3-A1_BASE)/4)) 	; B_I3
-	shrq	#8,r26
-	store	r26,(r15+((B_IINC-A1_BASE)/4)) 	; B_IINC
+	set_i_pixel
 	;; 
 	movefa	r18,r30					; blitter flags
 	movei	#PATDSEL|GOURD,r29
@@ -825,7 +843,7 @@ renderer:
  	or	r21,r28		; 1|w (executed during wait loop)
 	;; set z
 	addq	#32,r15
-	set_z
+	set_z_phrase
 	subq	#32,r15
 	;; 
  	movei	#PATDSEL|ZBUFF|DSTEN|DSTENZ|DSTWRZ|ZCOND,r29
@@ -840,30 +858,74 @@ renderer:
 .gouraud_zbuffer:
 	moveta	r28,r27		; save w
 	moveta	r27,r26		; save y|x1
-	;; compute z
-	compute_z
-	moveta	r25,r24		; save z
-	moveta	r26,r25		; save dz
 	;; compute i
 	compute_i
+	;;
+	detect_gouraud_mode	.gouraud_zbuffer_pixel_mode
+	;;
+	moveta	r25,r24		; save i
+	moveta	r26,r25		; save di
+	;; compute z
+	compute_z
 	;; 
 	movefa	r26,r27		; restore y|x1
 	movefa	r27,r28		; restore w
 	wait_blitter_gpu	r15,r29
  	or	r21,r28		; 1|w (executed during wait loop)
-	;; set intensities
-	move	r25,r30
-	set_i
-	addq	#32-12,r15
 	;; set z
-	movefa	r24,r25		; restore z
-	movefa	r25,r26		; restore dz
-	set_z
-	subq	#32,r15
+	addq	#32,r15
+	set_z_phrase
 	;; 
+	movefa	r24,r25		; restore i
+	movefa	r25,r26		; restore di
+	;; set intensities
+	set_i_phrase
+	;;
+	movefa	r18,r30					; blitter flags 
  	movei	#PATDSEL|GOURD|ZBUFF|DSTEN|DSTENZ|DSTWRZ|ZCOND,r29
 	store	r27,(r15+((A1_PIXEL-A1_BASE)/4))	; A1_PIXEL
 	store	r28,(r15+((B_COUNT-A1_BASE)/4))		; B_COUNT
+	store	r30,(r15+((A1_FLAGS-A1_BASE)/4))	; A1_FLAGS 
+	store	r29,(r15+((B_CMD-A1_BASE)/4))		; B_CMD
+	;; 
+	add	r10,r9		; lx += ldx
+	add	r12,r11		; rx += rdx
+	jump	(r18)		; -> .do_scanlines
+	add	r21,r4		; y++
+.gouraud_zbuffer_pixel_mode:
+	;; mandatory!
+	begin_gouraud_pixel
+	;;
+	moveq	#3,r30
+	moveta	r25,r24		; save i
+	and	r29,r30		; 3 - x1%4
+	moveta	r26,r25		; save di
+	shlq	#16,r30
+	sub	r30,r24		; adjust frac
+	;;
+	compute_z
+	;; 
+	;; 
+	movefa	r26,r27		; restore y|x1
+	movefa	r27,r28		; restore w
+	wait_blitter_gpu	r15,r29
+ 	or	r21,r28		; 1|w (executed during wait loop)
+	;; set z
+	addq	#32,r15
+	set_z_pixel
+	;; 
+	movefa	r24,r25		; restore i
+	movefa	r25,r26		; restore di
+	;; set intensities
+	subq	#32,r15
+	set_i_pixel
+	;;
+	movefa	r18,r30					; blitter flags 
+ 	movei	#PATDSEL|GOURD|ZBUFF|DSTEN|DSTENZ|DSTWRZ|ZCOND,r29
+	bset	#XADDPIX_BIT,r30
+	store	r27,(r15+((A1_PIXEL-A1_BASE)/4))	; A1_PIXEL
+	store	r28,(r15+((B_COUNT-A1_BASE)/4))		; B_COUNT
+	store	r30,(r15+((A1_FLAGS-A1_BASE)/4))	; A1_FLAGS 
 	store	r29,(r15+((B_CMD-A1_BASE)/4))		; B_CMD
 	;; 
 	add	r10,r9		; lx += ldx
@@ -1004,7 +1066,7 @@ renderer:
  	or	r21,r28		; 1|w (executed during wait loop)
 	;; set z
 	addq	#32,r15
-	set_z	
+	set_z_phrase	
 	subq	#32,r15
 	;;
  	movei	#SRCEN|LFU_REPLACE|ZBUFF|DSTEN|DSTENZ|DSTWRZ|ZCOND,r29	
