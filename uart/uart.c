ccfdf31796756dc61c80e7563dfbc47dcc3107e5
Seb/The Removers <SebRmv@jagware.org>
2011-10-26 08:40:17 +0200

    UART driver

--- uart/uart.c
+++ uart/uart.c
@@ -35,7 +35,84 @@ static const struct baudrate_entry_t baudrate_table[] = {
   { bd: B115200, clk: BAUDRATE(115200) }
 };
 
+#define arraysizeof(a$) (sizeof(a$) / sizeof(a$[0]))
+
+static uint16_t get_clk(enum uart_baudrate_t bd) {
+  int i;
+  for(i = 0; i < arraysizeof(baudrate_table); i++) {
+    if(baudrate_table[i].bd == bd) {
+      return baudrate_table[i].clk;
+    }
+  }
+  return baudrate_table[0].clk;
+}
+
+static uint16_t get_ctrl(enum uart_baudrate_t p) {
+  switch(p) {
+  case PODD:
+    return ASI_ODD|ASI_PAREN;
+    break;
+  case PEVEN:
+    return ASI_PAREN;
+    break;
+  case PNONE:
+  default:
+    return 0;
+  }
+}
+
+static int putc(FILE *fp, int c) {
+  while(!(JERRYREGS->asistat & ASI_TBE)) {
+  }
+  JERRYREGS->asidata = c & 0xff;
+  return c;
+}
+
+static int eof(FILE *fp) {
+  return 0;
+}
+
+static int getc(FILE *fp) {
+  return EOF;
+}
+
+static int flush(FILE *fp) {
+  while(!(JERRYREGS->asidata & ASI_TBE)) {
+  }
+  return 0;
+}
+
 FILE *open_uart(enum uart_baudrate_t bd, enum uart_parity_t p) {
-  
+  FILE *fp = calloc(1, sizeof(FILE));
+  if(fp == NULL) {
+    return NULL;
+  }
+
+  JERRYREGS->asiclk = get_clk(bd);
+  JERRYREGS->asictrl = ASI_CLRERR;
+  {
+    uint16_t tmp;
+    tmp = JERRYREGS->asidata;
+    tmp = JERRYREGS->asidata;
+  }
+  JERRYREGS->asictrl = get_ctrl(p);
+
+  fp->data = NULL;
+  // output actions
+  fp->putc = putc;
+  fp->puts = NULL; // use default implementation
+  fp->write = NULL; // use default implementation
+  // input actions
+  fp->getc = getc;
+  fp->gets = NULL; // use default implementation
+  fp->read = NULL; // use default implementation
+  //
+  fp->seek = NULL;
+  fp->tell = NULL;
+  fp->eof = eof;
+  fp->flush = flush;
+  fp->close = NULL;
+
+  return fp;
 }
 
