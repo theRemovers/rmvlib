ae2f4534a29869e5c4528cf2a8b2c03b5277db27
Seb/The Removers <SebRmv@jagware.org>
2011-10-26 08:09:50 +0200

    Starting UART driver

---
+++ uart/uart.h
@@ -0,0 +1,26 @@
+/* The Removers'Library */
+/* Copyright (C) 2006-2011 Seb/The Removers */
+/* http://removers.atari.org/ */
+
+/* This library is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU Lesser General Public */
+/* License as published by the Free Software Foundation; either */
+/* version 2.1 of the License, or (at your option) any later version. */
+
+/* This library is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public */
+/* License along with this library; if not, write to the Free Software */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA */
+
+/** \file uart.h
+ * \brief FILE wrapper to communicate on UART
+ */
+
+#ifndef _UART_H
+#define _UART_H
+
+#endif
